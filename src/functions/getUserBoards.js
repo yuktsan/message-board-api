0baf196aa619e9e35755dfeb4dc4dc46a860c58f
Caleb Chen <caleb.chen@peopleplan.com.au>
2021-06-16 11:53:49 +1000

    Add a getUserBoards functions

---
+++ src/functions/getUserBoards.js
@@ -0,0 +1,35 @@
+'use strict';
+const AWS = require('aws-sdk');
+
+
+module.exports.getUserBoards = async (event, context) => {
+  const userId = event.pathParameters.userId;
+
+  const payload = {
+      TableName: process.env.DYNAMODB_MESSAGE_BOARD_TABLE,
+      FilterExpression: "#messages.#userId = :keyUserId",
+      ExpressionAttributeNames: {
+        '#messages': 'messages',
+        '#userId': 'userId',
+      },
+      ExpressionAttributeValues: { ':keyUserId': userId}
+  };
+  try {
+    const dynamoDB = new AWS.DynamoDB.DocumentClient();
+    const results = await dynamoDB.scan(payload).promise();
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Headers': 'Authorization'
+      },
+      body: JSON.stringify({results})
+    }
+  } catch(error) {
+    console.log('User payload', payload);
+    return new Error('There was an error in getUserBoards' , error);
+  }
+
+
+};
